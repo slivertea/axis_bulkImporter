782fdb7e4e4b736be3f9918e716881380dfd28ac
sliver <slivertea@gmail.com>
2024-06-25 13:58:33 -0700

    init

---
+++ AxisImporter.py
@@ -0,0 +1,103 @@
+import sys
+import csv
+import json
+defaultsource = "example-source-ui.json"
+defaultfilename = "bulk_import-NR.csv"
+schemaNR = {
+    'Name': None,
+    'IP Ranges': None,
+    'DNS Searches': None,
+    'ICMP enabled (Optional)': None,
+    'Allowed Ports & Protocols': None,
+    'Connector Zone': None,
+    'Tags (Optional)': None
+}
+
+
+
+
+def setNet(arg1):
+    if "nativeIPRangesOrCIDRs" in arg1.keys():
+        nets = ";".join(arg1["nativeIPRangesOrCIDRs"])
+        return nets
+    else:
+        return None
+
+def setDomain(arg1):
+    if "nativeDnsSearches" in arg1.keys():
+        domains = ";".join(arg1["nativeDnsSearches"])
+        return domains
+    else:
+        return None
+
+def setPorts(arg1):
+    if "nativeTypedPortRanges" in arg1.keys():
+        portList = []
+        for range in arg1["nativeTypedPortRanges"]:
+            port = range["range"]
+            proto = range["protocol"].lower()
+            if proto == "all":
+                allPort = port
+            else:
+                allPort = port + ":" + proto
+            portList.append(allPort)
+        ports = ";".join(portList)
+        return ports
+    else:
+        return None
+
+def setZone(arg1):
+    return arg1["connectorZone"]["name"]
+
+
+def setTags(arg1):
+    return None
+
+
+def parse(argv):
+    parseList = []
+    for app in argv:
+        dict = {
+            'Name': None,
+            'IP Ranges': None,
+            'DNS Searches': None,
+            'ICMP enabled (Optional)': None,
+            'Allowed Ports & Protocols': None,
+            'Connector Zone': None,
+            'Tags (Optional)': None
+        }
+        if app["applicationProtocol"] == "Native" and app["applicationEntityType"] == "Legacy":
+            dict["Name"] = app["name"]
+            if app["isIcmpEnabled"]:
+                dict["ICMP enabled (Optional)"] = 'true'
+            else:
+                dict["ICMP enabled (Optional)"] = 'false'
+            dict["Connector Zone"] = setZone(app)
+            dict['IP Ranges'] = setNet(app)
+            dict['DNS Searches'] = setDomain(app)
+            dict['Allowed Ports & Protocols'] = setPorts(app)
+            dict['Tags (Optional)'] = setTags(app)
+
+        parseList.append(dict)
+
+    return parseList
+
+
+def write(argv, filename=defaultfilename):
+    with open(filename, 'w') as csvFh:
+        writer = csv.DictWriter(csvFh, fieldnames=schemaNR.keys())
+        writer.writeheader()
+        writer.writerows(argv)
+
+
+def main(argv):
+    ### FIle Creation(s)
+    # Dump the pareList array into the axis bulk import template for Network Ranges
+    with open (defaultsource) as jf:
+        appSource = json.load(jf)
+    parsedList = parse(appSource)
+    write(parsedList)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # axis_bulkImporter
 Convert UI json objects to Bulk-Import templates
+
+Use this tool to create bulk import csv files for import to the Axis portal.  
+Source data by default is the Applications json data file taken from the UI via browser debug tools
+
+This tool can be imported to any other python script/tool to parse source data
+into the correct schema necessary for bulk import.
+
+Example - Migrating applications from one tenant to another, or customer, or service.
+
+Requirement:
+* Source data in json format.  
